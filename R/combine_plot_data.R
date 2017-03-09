c8a723fadcdf9580dcf3c577acbd7fa005b02224
Eduard Bukin <edwardbukin@gmail.com>
2017-03-09 19:00:45 +0100

    Visual QA

---
+++ R/combine_plot_data.R
@@ -0,0 +1,26 @@
+# combining and plotting data on important countries
+combine_plot_data <-
+  function(outlook,
+           faostat,
+           areas = c("WLD", "COS", "CHN", "KHM", "IDN", "LAO", "MYS", "MMR", "PHL", "THA", "VNM"),
+           n_page = 12) {
+    dfp <-
+      outlook %>%
+      filter(AreaCode %in% unique(fsActivityData$AreaCode)) %>%
+      mutate(d.source = "Outlook") %>%
+      bind_rows(faostat %>%
+                  mutate(d.source = "Faostat"))
+    
+    if (any(!is.na(areas))) {
+      dfp <-
+        dfp %>%
+        filter(AreaCode %in% areas)
+    }
+    
+    plot_group(dfp,
+               n_page = n_page,
+               groups_var = c("ElementCode", "ItemCode"),
+               plots_var = "AreaCode")
+    
+  }
+

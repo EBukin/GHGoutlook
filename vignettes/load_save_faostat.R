2cd45750a7d3ebe8ab6dc28577f5c45d75082b49
Eduard Bukin <edwardbukin@gmail.com>
2017-03-10 13:47:08 +0200

    Data loading update

--- vignettes/load_save_faostat.R
+++ vignettes/load_save_faostat.R
@@ -19,7 +19,7 @@ l_ply(str_c("R/", list.files("R/", pattern="*.R")), source)
 # Loading data ------------------------------------------------------------
 
 
-filse <- 
+files <- 
   c("data/Emissions_Agriculture_Agriculture_total_E_All_Data_(Norm).csv",
     "data/Emissions_Agriculture_Burning_crop_residues_E_All_Data_(Norm).csv",
     "data/Emissions_Agriculture_Burning_Savanna_E_All_Data_(Norm).csv",
@@ -30,30 +30,52 @@ filse <-
     "data/Emissions_Agriculture_Manure_left_on_pasture_E_All_Data_(Norm).csv",
     "data/Emissions_Agriculture_Manure_Management_E_All_Data_(Norm).csv",
     "data/Emissions_Agriculture_Rice_Cultivation_E_All_Data_(Norm).csv",
-    "data/Emissions_Agriculture_Burning_crop_residues_E_All_Data_(Norm).csv",
     "data/Emissions_Land_Use_Burning_Biomass_E_All_Data_(Norm).csv",
     "data/Emissions_Land_Use_Cropland_E_All_Data_(Norm).csv",
     "data/Emissions_Land_Use_Forest_Land_E_All_Data_(Norm).csv",
     "data/Emissions_Land_Use_Grassland_E_All_Data_(Norm).csv",
     "data/Emissions_Land_Use_Land_Use_Total_E_All_Data_(Norm).csv")
 
+domains <- 
+  c("GT",
+    "GB",
+    "GH",
+    "GA",
+    "GV",
+    "GE",
+    "GU",
+    "GP",
+    "GM",
+    "GR",
+    "GI",
+    "GC",
+    "GF",
+    "GG",
+    "GL")
+
 
 fs <- 
-  llply(filse, read.fs.bulk) %>% 
-  bind_rows() %>% 
+  ddply(tibble(files, domains), 
+        .(files), 
+        function(x) {
+          if(file.exists(x$files)) {
+            read.fs.bulk(x$files) %>% mutate(Domain = as.character(x$domains))
+            }
+          }) %>% 
+  # bind_rows() %>% 
   tbl_df()
 
 # Exporting items and elements
 els <- fs %>%
-  select(ElementCode, ElementName, Unit) %>% 
+  select(Domain, ElementCode, ElementName, Unit) %>% 
   distinct()
 
 its <- fs %>%
-  select(ItemCode, ItemName) %>% 
+  select(Domain, ItemCode, ItemName) %>% 
   distinct()
 
-fs <- 
+fs <-
   fs %>% 
-  select(AreaCode, ItemCode, ElementCode, Year, Value)
+  select(Domain, AreaCode, ItemCode, ElementCode, Year, Value, Unit, ElementName, ItemName)
 
 save(fs, its, els, file = "data/all_fs_emissions.Rdata")

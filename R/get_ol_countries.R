f8a051ff21053ea29b5cad60990bf1af661844c6
Eduard Bukin <edwardbukin@gmail.com>
2017-03-24 09:08:28 +0200

    Get OL function

---
+++ R/get_ol_countries.R
@@ -0,0 +1,13 @@
+get_ol_countries <- function() {
+  c("AF1", "AF2", "AGO", "ARG", "AS2", "ASA", "ASD", "AUS", "BFA", "BGD",
+    "BRA", "CAN", "CHE", "CHL", "CHN", "CIV", "CMR", "COD", "COG", "COL",
+    "DZA", "E15", "EGY", "ETH", "EUE", "EUW", "GAB", "GHA", "HTI", "IDN",
+    "IND", "IRN", "IRQ", "ISR", "JPN", "KAZ", "KEN", "KHM", "KOR", "LAO",
+    "LBN", "LBY", "MAR", "MDG", "MEX", "MLI", "MMR", "MOZ", "MRT", "MWI",
+    "MYS", "NGA", "NMS", "NOR", "NRE", "NZL", "OCE", "OCL", "PAK", "PER",
+    "PHL", "PRY", "RUS", "RWA", "SAC", "SAU", "SDN", "SEN", "SOM", "TCD",
+    "THA", "TUN", "TUR", "TZA", "UGA", "UKR", "URY", "USA", "VNM", #"WLD",
+    "YEM", "ZAF", "ZMB", "ZWE") %>% 
+    return()
+}
+
